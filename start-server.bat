89201188c162756473e547167b9809238a0cbda3
arjunacharya-aa <arjunacharya290@gmail.com>
2025-12-06 12:45:09 +0545

    Add files via upload

---
+++ start-server.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Starting HTTP Server on http://localhost:8000/
+echo Opening browser...
+start http://localhost:8000/
+powershell -ExecutionPolicy Bypass -File start-server.ps1
+
+
+
+
+
